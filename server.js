b2771d0b52c6d11aa79fccd9dd7ba2cbb3206bf5
mroshan2003 <mra7862003@gmail.com>
2026-02-02 23:15:20 +0530

    Cors updated

--- server.js
+++ server.js
@@ -9,13 +9,6 @@ import portfolioRoutes from "./routes/portfolioRoutes.js";
 dotenv.config();
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://127.0.0.1:5173",
-  "https://clipcraftmedia.in",
-  "https://www.clipcraftmedia.in"
-];
-
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin) return callback(null, true);
