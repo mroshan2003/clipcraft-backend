db34a2f860e84a3ef0d82a7c1479b2e91dc92209
mroshan2003 <mra7862003@gmail.com>
2026-02-02 17:28:46 +0530

    Cors updated

--- server.js
+++ server.js
@@ -9,12 +9,29 @@ import portfolioRoutes from "./routes/portfolioRoutes.js";
 dotenv.config();
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+  "https://clipcraftmedia.in",
+  "https://www.clipcraftmedia.in"
+];
+
 app.use(cors({
-  origin: [
-    "https://clipcraftmedia.in",
-    "https://www.clipcraftmedia.in"
-  ]
+  origin: function (origin, callback) {
+    // allow requests with no origin (Postman, curl)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("CORS not allowed"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
 app.use(express.json());
 
 mongoose
